a096bcf8fd0aee588192da0c408c51e90503a432
mazgaed <145921657+mazgaed@users.noreply.github.com>
2023-12-17 20:45:06 +0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,7 +1,27 @@
-﻿string[] arr = { "hello", "world", "^^", ":D", "GeekBrains" }; // Исходный массив
+string[] arr = { "hello", "world", "^^", ":D", "GeekBrains" }; // Исходный массив
 
-foreach (string s in arr){ // Ищем строки, которые больше 3 символов
-if(s.Length > 3){
-  System.Console.WriteLine(s); // Выводим элементы массива
+int count = 0;
+foreach (string s in arr)           // Ищем строки, которые больше 3 символов
+{
+  if (s.Length <= 3)
+  {
+    count++;
+  }
 }
+// Создаем новый массив из строк, длина которых меньше или равна 3 символам
+string[] newArr = new string[count];
+int index = 0;
+foreach (string s in arr)
+{
+  if (s.Length <= 3)
+  {
+    newArr[index] = s;
+    index++;
+  }
+}
+
+// Выводим новый массив
+foreach (string s in newArr)
+{
+  Console.WriteLine(s);
 }
